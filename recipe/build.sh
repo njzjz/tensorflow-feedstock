0f004cae80b1a73a555cabed224594d32ecca782
Isuru Fernando <isuruf@gmail.com>
2026-04-28 13:53:02 -0700

    rm -rf include/python

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,15 +9,14 @@ for ver in 3.9 3.10 3.11 3.12; do
   export PY_VER=$ver
   echo "Building for $PY_VER"
   date
-  if [[ "$PY_VER" == "3.9" ]]; then
-    # let bazel download python headers first
-    bash $RECIPE_DIR/build_common.sh || true
-  fi
+  # let bazel download python headers first
+  bash $RECIPE_DIR/build_common.sh || true
   output_base=$(bazel info output_base)
   python_h_path=$(find $output_base/external -wholename "*/python${PY_VER}/Python.h")
   rm -rf $PREFIX/include/python
   # copy the bazel downloaded headers to PREFIX
   cp -r $(dirname $python_h_path) $PREFIX/include/python
   bash $RECIPE_DIR/build_common.sh
+  rm -rf $PREFIX/include/python
 done
 bazel clean
