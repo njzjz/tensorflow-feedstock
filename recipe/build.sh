a9104bab9bc18d73351925209cbb920e7df5bdac
Isuru Fernando <isuruf@gmail.com>
2026-04-28 10:27:45 -0700

    download python headers for 3.9 only

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,10 @@ for ver in 3.9 3.10 3.11 3.12; do
   export PY_VER=$ver
   echo "Building for $PY_VER"
   date
-  # let bazel download python headers first
-  bash $RECIPE_DIR/build_common.sh || true
+  if [[ "$PY_VER == "3.9" ]]; then
+    # let bazel download python headers first
+    bash $RECIPE_DIR/build_common.sh || true
+  fi
   output_base=$(bazel info output_base)
   python_h_path=$(find $output_base/external -wholename "*/python${PY_VER}/Python.h")
   rm -rf $PREFIX/include/python
